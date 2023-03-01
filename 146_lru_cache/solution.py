915981bcf1a5657a01c3c265c6938169878ce4bd
Blake <bd.yhome@gmail.com>
2023-03-01 12:23:47 -0600

    fixed 146

--- 146_lru_cache/solution.py
+++ 146_lru_cache/solution.py
@@ -1,36 +1,60 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
 class LRUCache(object):
 
     def __init__(self, capacity):
         """
         :type capacity: int
         """
-        #initialize hashmap
-        self.hashmap = {}
-
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
+        self.cache = {}
         self.capacity = capacity
 
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        self.left = Node(0,0)
+        self.right = Node(0,0)
 
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    ##helper functions for linked list management##########
     
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def insert(self, node):
+
+        node.prev = self.right.prev
+        node.next = self.right
+
+        self.right.prev.next = node
+        self.right.prev = node
+
+
+
+
+
+    ##################
+ 
+
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        if key in self.hashmap:
-            self.remove(self.hashmap[key])
-            self.insert(self.hashmap[key])
-            return self.hashmap[key].val
+        #if it exists in the cache, return it
+        if key in self.cache:
+            #move to the right all the way on linked list because it has been accessed
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
         return -1
+        
 
     def put(self, key, value):
         """
@@ -38,16 +62,20 @@ class LRUCache(object):
         :type value: int
         :rtype: None
         """
-        if key in self.hashmap:
-            self.remove(self.hashmap[key])
-        self.hashmap[key] = Node(key, value)
-        self.insert(self.hashmap[key])
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
 
-        if len(self.hashmap) > self.capacity:
-            #remove from the linked list and delete the LRU from the hashmap
+        if len(self.cache) > self.capacity:
             lru = self.left.next
+            #remove the left node
             self.remove(lru)
-            del self.hashmap[lru.key]
+
+            #remove from the hashmap
+            del self.cache[lru.key]
+        
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
